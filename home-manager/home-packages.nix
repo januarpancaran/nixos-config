322ef4193525236ea264102beaf7787cfb8c8a25
januarpancaran <januar352@gmail.com>
2025-01-28 22:34:42 +0700

    adding home packages

--- home-manager/home-packages.nix
+++ home-manager/home-packages.nix
@@ -4,9 +4,45 @@
   nixpkgs.config.allowUnfree = true;
 
   home.packages = with pkgs; [
+    bat
+    blueberry
+    brightnessctl
+    curl
+    dconf
+    dunst
+    eog
+    fastfetch
+    firefox
     git
+    glib
+    grim
+    htop
+    jq
+    killall
     nautilus
     neovim
+    networkmanagerapplet
+    obs-studio
+    playerctl
     polkit_gnome
+    rofi-wayland
+    slurp
+    spotify
+    swww
+    telegram-desktop
+    trash-cli
+    unrar
+    unzip 
+    webcord
+    wget
+    wl-clipboard
+    wlogout
+    xwaylandvideobridge
+    zoxide
   ];
+
+  programs.mpv = {
+    package = pkgs.mpv;
+    enable = true;
+  };
 }
