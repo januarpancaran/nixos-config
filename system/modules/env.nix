8f0bcb35a4ecab2577925f7d9966fba53c21eb9d
januarpancaran <januar352@gmail.com>
2025-01-30 07:03:41 +0700

    adding env variables

--- system/modules/env.nix
+++ system/modules/env.nix
@@ -1,5 +1,14 @@
 {
   environment.sessionVariables = {
+    XCURSOR_SIZE = 24;
+    HYPRCURSOR_SIZE = 24;
+    XDG_CURRENT_DESKTOP = "Hyprland";
+    XDG_SESSION_DEKSTOP = "Hyprland";
+    XDG_SESSION_TYPE = "wayland";
+    QT_QPA_PLATFORMTHEME = "qt6ct";
+    QT_QPA_PLATFORM = "wayland; xcb";
+    GDK_BACKEND = "wayland, x11, *";
+    ELECTRON_OZONE_PLATFORM_THEME = "wayland";
     NIXOS_OZONE_WL = 1;
   };
 }
