440039f6d62fca1ce2fc6f01a11a0e4d8c1e30d0
januarpancaran <januar352@gmail.com>
2025-03-11 14:25:29 +0700

    adding apparmor

--- system/configuration.nix
+++ system/configuration.nix
@@ -19,20 +19,11 @@ in {
 
   time.timeZone = "Asia/Jakarta";
 
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
   console = {
     font = "Lat2-Terminus16";
     keyMap = "us";
-    #   useXkbConfig = true; # use xkb.options in tty.
   };
-
-  # services.xserver.enable = true;
-  # services.xserver.xkb.layout = "us";
-  # services.xserver.xkb.options = "eurosign:e,caps:escape";
 
   services.printing.enable = true;
   services.libinput.enable = true;
@@ -46,27 +37,9 @@ in {
     trash-cli
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # Enable the OpenSSH daemon.
   services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
   networking.firewall.enable = true;
-
-  # Copy the NixOS configuration file and link it from the resulting system
-  # (/run/current-system/configuration.nix). This is useful in case you
-  # accidentally delete configuration.nix.
-  # system.copySystemConfiguration = true;
+  security.apparmor.enable = true;
 
   system.stateVersion = "24.11";
 }
