e94a331a2b5721d26793ed3a3e1944e82987699d
januarpancaran <januar352@gmail.com>
2025-03-22 17:56:52 +0700

    slight naming change on nvim config

--- home-manager/modules/nvim/lua/plugin/default.nix
+++ home-manager/modules/nvim/lua/plugin/default.nix
@@ -3,213 +3,215 @@
   inputs,
   ...
 }: {
-  programs.neovim.plugins = with pkgs.vimPlugins; let
-    read = file: "${builtins.readFile file}";
-  in [
-    {
-      plugin = alpha-nvim;
-      type = "lua";
-      config = ''
-        require("alpha").setup(require("alpha.themes.dashboard").config)
-      '';
-    }
+  programs.neovim = {
+    plugins = with pkgs.vimPlugins; let
+      read = file: "${builtins.readFile file}";
+    in [
+      {
+        plugin = alpha-nvim;
+        type = "lua";
+        config = ''
+          require("alpha").setup(require("alpha.themes.dashboard").config)
+        '';
+      }
 
-    {
-      plugin = comment-nvim;
-      type = "lua";
-      config = ''
-        require("Comment").setup()
-      '';
-    }
+      {
+        plugin = comment-nvim;
+        type = "lua";
+        config = ''
+          require("Comment").setup()
+        '';
+      }
 
-    # Completions
-    {
-      plugin = nvim-cmp;
-      type = "lua";
-      config = read ../../after/plugin/completions.lua;
-    }
-    cmp-nvim-lsp
-    luasnip
-    cmp_luasnip
-    friendly-snippets
+      # Completions
+      {
+        plugin = nvim-cmp;
+        type = "lua";
+        config = read ../../after/plugin/completions.lua;
+      }
+      cmp-nvim-lsp
+      luasnip
+      cmp_luasnip
+      friendly-snippets
 
-    # Git
-    {
-      plugin = gitsigns-nvim;
-      type = "lua";
-      config = read ../../after/plugin/gitsigns.lua;
-    }
+      # Git
+      {
+        plugin = gitsigns-nvim;
+        type = "lua";
+        config = read ../../after/plugin/gitsigns.lua;
+      }
 
-    # Indent
-    {
-      plugin = indent-blankline-nvim;
-      type = "lua";
-      config = read ../../after/plugin/indent-blankline.lua;
-    }
-    {
-      plugin = rainbow-delimiters-nvim;
-      type = "lua";
-      config = read ../../after/plugin/rainbow-delimiters.lua;
-    }
+      # Indent
+      {
+        plugin = indent-blankline-nvim;
+        type = "lua";
+        config = read ../../after/plugin/indent-blankline.lua;
+      }
+      {
+        plugin = rainbow-delimiters-nvim;
+        type = "lua";
+        config = read ../../after/plugin/rainbow-delimiters.lua;
+      }
 
-    # Lspconfig
-    {
-      plugin = nvim-lspconfig;
-      type = "lua";
-      config = read ../../after/plugin/lsp-config.lua;
-    }
+      # Lspconfig
+      {
+        plugin = nvim-lspconfig;
+        type = "lua";
+        config = read ../../after/plugin/lsp-config.lua;
+      }
 
-    # Lualine
-    {
-      plugin = lualine-nvim;
-      type = "lua";
-      config = ''
-        require("lualine").setup({
-          options = {
-            theme = "dracula",
-          }
-        })
-      '';
-    }
-    nvim-web-devicons
+      # Lualine
+      {
+        plugin = lualine-nvim;
+        type = "lua";
+        config = ''
+          require("lualine").setup({
+            options = {
+              theme = "dracula",
+            }
+          })
+        '';
+      }
+      nvim-web-devicons
 
-    # Markdown Preview
-    {
-      plugin = markdown-preview-nvim;
-      type = "lua";
-      config = read ../../after/plugin/markdown-preview.lua;
-    }
+      # Markdown Preview
+      {
+        plugin = markdown-preview-nvim;
+        type = "lua";
+        config = read ../../after/plugin/markdown-preview.lua;
+      }
 
-    # None ls
-    {
-      plugin = none-ls-nvim;
-      type = "lua";
-      config = read ../../after/plugin/none-ls.lua;
-    }
-    (pkgs.vimUtils.buildVimPlugin {
-      name = "none-ls-extras-nvim";
-      src = inputs.none-ls-extras;
-      dependencies = [none-ls-nvim];
-    })
+      # None ls
+      {
+        plugin = none-ls-nvim;
+        type = "lua";
+        config = read ../../after/plugin/none-ls.lua;
+      }
+      (pkgs.vimUtils.buildVimPlugin {
+        name = "none-ls-extras-nvim";
+        src = inputs.none-ls-extras;
+        dependencies = [none-ls-nvim];
+      })
 
-    # Vim tmux navigator
-    {
-      plugin = vim-tmux-navigator;
-      type = "lua";
-      config = read ../../after/plugin/vim-tmux-navigator.lua;
-    }
+      # Vim tmux navigator
+      {
+        plugin = vim-tmux-navigator;
+        type = "lua";
+        config = read ../../after/plugin/vim-tmux-navigator.lua;
+      }
 
-    # Telescope
-    {
-      plugin = telescope-nvim;
-      type = "lua";
-      config = read ../../after/plugin/telescope.lua;
-    }
-    telescope-ui-select-nvim
-    plenary-nvim
+      # Telescope
+      {
+        plugin = telescope-nvim;
+        type = "lua";
+        config = read ../../after/plugin/telescope.lua;
+      }
+      telescope-ui-select-nvim
+      plenary-nvim
 
-    # Theme
-    {
-      plugin = catppuccin-nvim;
-      type = "lua";
-      config = ''
-        vim.cmd.colorscheme("catppuccin-mocha")
-      '';
-    }
+      # Theme
+      {
+        plugin = catppuccin-nvim;
+        type = "lua";
+        config = ''
+          vim.cmd.colorscheme("catppuccin-mocha")
+        '';
+      }
 
-    # Treesitter
-    {
-      plugin = nvim-treesitter.withAllGrammars;
-      type = "lua";
-      config = read ../../after/plugin/treesitter.lua;
-    }
+      # Treesitter
+      {
+        plugin = nvim-treesitter.withAllGrammars;
+        type = "lua";
+        config = read ../../after/plugin/treesitter.lua;
+      }
 
-    # Trouble
-    {
-      plugin = trouble-nvim;
-      type = "lua";
-      config = read ../../after/plugin/trouble-nvim.lua;
-    }
+      # Trouble
+      {
+        plugin = trouble-nvim;
+        type = "lua";
+        config = read ../../after/plugin/trouble-nvim.lua;
+      }
 
-    # Undotree
-    {
-      plugin = undotree;
-      type = "lua";
-      config = ''
-        vim.keymap.set("n", "<leader>ut", vim.cmd.UndotreeToggle)
-      '';
-    }
-  ];
+      # Undotree
+      {
+        plugin = undotree;
+        type = "lua";
+        config = ''
+          vim.keymap.set("n", "<leader>ut", vim.cmd.UndotreeToggle)
+        '';
+      }
+    ];
 
-  programs.neovim.extraPackages = with pkgs; [
-    # Lsp
-    bash-language-server
-    clang-tools
-    docker-compose-language-service
-    dockerfile-language-server-nodejs
-    eslint
-    gopls
-    jdt-language-server
-    lua-language-server
-    marksman
-    matlab-language-server
-    nixd
-    pyright
-    rust-analyzer
-    sqls
-    svelte-language-server
-    tailwindcss-language-server
-    texlab
-    typescript-language-server
-    vscode-langservers-extracted
-    vue-language-server
+    extraPackages = with pkgs; [
+      # Lsp
+      bash-language-server
+      clang-tools
+      docker-compose-language-service
+      dockerfile-language-server-nodejs
+      eslint
+      gopls
+      jdt-language-server
+      lua-language-server
+      marksman
+      matlab-language-server
+      nixd
+      pyright
+      rust-analyzer
+      sqls
+      svelte-language-server
+      tailwindcss-language-server
+      texlab
+      typescript-language-server
+      vscode-langservers-extracted
+      vue-language-server
 
-    # None ls
-    # Shell
-    shfmt
+      # None ls
+      # Shell
+      shfmt
 
-    # Lua
-    stylua
+      # Lua
+      stylua
 
-    # Javascript
-    eslint_d
-    jq
-    prettierd
+      # Javascript
+      eslint_d
+      jq
+      prettierd
 
-    # Css
-    stylelint
+      # Css
+      stylelint
 
-    # Markdown
-    markdownlint-cli2
+      # Markdown
+      markdownlint-cli2
 
-    # Golang
-    go-tools
-    asmfmt
-    golangci-lint
-    golangci-lint-langserver
-    golines
-    gofumpt
+      # Golang
+      go-tools
+      asmfmt
+      golangci-lint
+      golangci-lint-langserver
+      golines
+      gofumpt
 
-    # Cpp
-    cpplint
-    cppcheck
+      # Cpp
+      cpplint
+      cppcheck
 
-    # Python
-    pylint
-    black
-    isort
+      # Python
+      pylint
+      black
+      isort
 
-    # Nix
-    alejandra
+      # Nix
+      alejandra
 
-    # Java
-    google-java-format
-    checkstyle
+      # Java
+      google-java-format
+      checkstyle
 
-    # LaTex
-    textidote
+      # LaTex
+      textidote
 
-    # Docker
-    hadolint
-  ];
+      # Docker
+      hadolint
+    ];
+  };
 }
