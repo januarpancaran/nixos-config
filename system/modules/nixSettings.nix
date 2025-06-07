b2527abae095985e60eb6eee02d40c3a888aea6c
januarpancaran <januar352@gmail.com>
2025-06-07 11:51:40 +0700

    del: removing auto garbage collection

--- system/modules/nixSettings.nix
+++ system/modules/nixSettings.nix
@@ -9,12 +9,6 @@
       experimental-features = ["nix-command" "flakes"];
       auto-optimise-store = true;
     };
-
-    gc = {
-      automatic = true;
-      dates = "weekly";
-      options = "--delete-older-than 7d";
-    };
   };
 
   nixpkgs.config.allowUnfree = true;
