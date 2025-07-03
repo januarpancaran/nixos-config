b74f01443488a3a254c523f50b9bc1eaecbb3daf
januarpancaran <januar352@gmail.com>
2025-07-03 11:51:58 +0700

    change: changing tlp settings

--- system/modules/tlp.nix
+++ system/modules/tlp.nix
@@ -5,6 +5,7 @@
     enable = true;
 
     settings = {
+      TLP_ENABLE = 1;
       TLP_DEFAULT_MODE = "BAT";
       TLP_PERSISTENT_DEFAULT = 0;
 
@@ -16,31 +17,32 @@
 
       MEM_SLEEP_ON_AC = "s2idle";
       MEM_SLEEP_ON_BAT = "deep";
-      PLATFORM_PROFILE_ON_AC = "performance";
-      PLATFORM_PROFILE_ON_BAT = "low-power";
-
+      
       SOUND_POWER_SAVE_ON_AC = 1;
       SOUND_POWER_SAVE_ON_BAT = 1;
+      
+      WIFI_PWR_ON_AC = "on";
+      WIFI_PWR_ON_BAT = "on";
+
+      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_BAT = "low-power";
 
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
       CPU_MIN_PERF_ON_AC = 0;
       CPU_MAX_PERF_ON_AC = 100;
       CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 20;
+      CPU_MAX_PERF_ON_BAT = 30;
 
       CPU_BOOST_ON_AC = 1;
       CPU_BOOST_ON_BAT = 0;
 
       CPU_HWP_DYN_BOOST_ON_AC = 1;
       CPU_HWP_DYN_BOOST_ON_BAT = 0;
-
-      WIFI_PWR_ON_AC = "on";
-      WIFI_PWR_ON_BAT = "on";
     };
   };
 }
