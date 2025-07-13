7127041f8a8d39f3452ac533e988fa117346fe17
januarpancaran <januar352@gmail.com>
2025-07-13 22:32:07 +0700

    add: adding japanese and korean keyboards

--- home-manager/modules/fcitx5.nix
+++ home-manager/modules/fcitx5.nix
@@ -10,6 +10,8 @@
           fcitx5-gtk
           fcitx5-nord
           fcitx5-chinese-addons
+          fcitx5-anthy
+          fcitx5-hangul
         ];
 
         settings = {
@@ -26,6 +28,8 @@
 
             "Groups/0/Items/0".Name = "keyboard-us";
             "Groups/0/Items/1".Name = "pinyin";
+            "Groups/0/Items/2".Name = "anthy";
+            "Groups/0/Items/3".Name = "hangul";
 
             GroupOrder."0" = "Default";
           };
